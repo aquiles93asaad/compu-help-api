53a90e951da9b5a72942dddf19168c786fe461bf
Aquiles Asaad <aquiles19939@gmail.com>
2019-10-31 11:47:33 -0300

    changed port for heroku to work

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('./src/config/mongoose');
 // module.parent check is required to support mocha watch
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
-    app.listen(config.port, () => {
+    app.listen(process.env.PORT || config.port, () => {
         console.info(`server started on port ${config.port} (${config.env})`);
     });
 }

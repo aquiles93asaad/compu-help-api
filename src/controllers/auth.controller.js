6721e969e3a2c60dd6c1a3f731ecdc6f5fbc08d6
Aquiles Asaad <aquiles19939@gmail.com>
2019-11-07 18:53:39 -0300

    fix token error

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,14 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
+const _ = require('lodash');
 
 module.exports = {
     generateToken
 }
 
 function generateToken(user) {
-    const payload = JSON.stringify(user);
+    let userToToken = _.clone(user);
+    delete userToToken.image;
+    const payload = JSON.stringify(userToToken);
     return jwt.sign(payload, config.jwtSecret);
 }
